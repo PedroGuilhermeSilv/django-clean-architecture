ee051659899d1b20ddbc001d64678b1138ed70e8
guilherme <vinespedro55@gmail.com>
2024-03-02 01:04:00 +0000

    test: :white_check_mark: Update unit test validators and settings.json

--- src/__seedwork/tests/unit/domain/test_unit_validators.py
+++ src/__seedwork/tests/unit/domain/test_unit_validators.py
@@ -92,3 +92,28 @@ class TestValidorRules(unittest.TestCase):
         self.assertIsInstance(
             ValidatorRules.values(value=value, prop=prop).boolean(), ValidatorRules
         )
+
+    def test_throw_a_validation_exception_when_two_or_more_rules(self):
+        with self.assertRaises(Exception) as assert_error:
+            ValidatorRules.values(value=None, prop="prop").required().string().boolean()
+        self.assertEqual("prop is required", str(assert_error.exception))
+
+        with self.assertRaises(Exception) as assert_error:
+            ValidatorRules.values(value=5, prop="prop").required().string()
+        self.assertEqual("prop must be a string", str(assert_error.exception))
+
+        with self.assertRaises(Exception) as assert_error:
+            ValidatorRules.values(value="test", prop="prop").required().max_length(3)
+        self.assertEqual(
+            "prop must be less than 3 characters", str(assert_error.exception)
+        )
+
+        with self.assertRaises(Exception) as assert_error:
+            ValidatorRules.values(value="test", prop="prop").string().boolean()
+        self.assertEqual("prop must be a boolean", str(assert_error.exception))
+
+    def test_case_valid_for_many_rules(self):
+        ValidatorRules.values(value="test", prop="prop").required().string()
+        ValidatorRules.values(value="test", prop="prop").required().max_length(4)
+        ValidatorRules.values(value=True, prop="prop").required().boolean()
+        ValidatorRules.values(value=False, prop="prop").required().boolean()
