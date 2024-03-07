b6c8e0574600bd3607160c126f06326686f20ce7
guilherme <vinespedro55@gmail.com>
2024-03-07 13:51:51 +0000

    feat: :sparkles: mock in test unit for entity Category

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ### Typing
 - É usada para adicionar tipos de dados opcionais e anotações de tipo ao código Python.
 
-## Testes:
+## Pirâmede de Teste:
 - Testes Unitários:
     - Objetivo: Testar unidades individuais de código, como funções ou métodos, isoladamente.
     - Escopo: Foca em partes específicas do código, verificando se cada unidade funciona conforme o esperado.
@@ -21,6 +21,19 @@
     - Escopo: Engloba todos os componentes do sistema, incluindo interfaces de usuário, servidores, bancos de dados e integrações externas.
     - Isolamento: Testa o sistema como um todo, sem isolar componentes individuais.
 
+## Ferramentas de teste:
+- Mocks:
+    - Um "mock" em Python é um objeto simulado que imita o comportamento de um objeto real em um contexto controlado. Ele é usado para substituir partes do código que não são desejáveis ou práticas para executar durante os testes.
+
+    - Substituição de Comportamento: Um mock pode ser usado para substituir o comportamento de objetos reais durante a execução dos testes. Isso é útil quando você quer isolar o código que está sendo testado e não quer que ele dependa de componentes externos, como bancos de dados, APIs externas, ou métodos que realizam operações caras ou imprevisíveis.
+
+    - Configuração de Comportamento: Você pode configurar um mock para retornar valores específicos quando chamado, simular exceções, contar quantas vezes foi chamado, verificar com quais argumentos foi chamado, entre outras coisas. Isso permite que você escreva testes específicos para diferentes cenários e comportamentos do seu código.
+
+    - Facilita Testes de Unidade: Usar mocks pode facilitar a escrita de testes de unidade, pois você pode isolar o código que está sendo testado e se concentrar em testar apenas a unidade específica, sem se preocupar com suas dependências externas.
+
+    - Evita Efeitos Colaterais: Ao substituir partes do código com mocks, você pode evitar efeitos colaterais indesejados durante a execução dos testes. Por exemplo, se um método real escreve em um banco de dados, usando um mock você pode evitar que o banco de dados seja modificado durante os testes.
+
+    - Ex: No projeto utilizamos o mock na função validate nos teste unitários da entidade category, para garantir que esse método não tenha influência sobre as funções da entidade.
 ## SeedWorks
 - São objetos que podem ser utilizados de em outras camadas de aplicação de forma a facilitar a implementação e reduzir duplicação de código.
 
--- src/category/tests/unit/domain/test_unit_entities.py
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import unittest
 from dataclasses import is_dataclass, FrozenInstanceError
 from category.domain.entities import Category
+from unittest.mock import patch
 
 
 class TestCategory(unittest.TestCase):
@@ -9,49 +10,58 @@ class TestCategory(unittest.TestCase):
         self.assertTrue(is_dataclass(Category))
 
     def test_constructor_default(self):
-        category = Category(name="Movie")
-        self.assertEqual(category.name, "Movie")
-        self.assertIsNone(category.description)
-        self.assertTrue(category.is_activate)
-        self.assertIsInstance(category.created_at, datetime)
+        with patch.object(Category, "validate"):
+            category = Category(name="Movie")
+            self.assertEqual(category.name, "Movie")
+            self.assertIsNone(category.description)
+            self.assertTrue(category.is_activate)
+            self.assertIsInstance(category.created_at, datetime)
 
     def test_contructor_with_all_params(self):
-        created_at = datetime.now()
-        category = Category(
-            name="Movie",
-            description="descrição",
-            is_activate=True,
-            created_at=created_at,
-        )
-        self.assertEqual(category.name, "Movie")
-        self.assertEqual(category.description, "descrição")
-        self.assertTrue(category.is_activate)
-        self.assertEqual(category.created_at, created_at)
+        with patch.object(Category, "validate"):
+            created_at = datetime.now()
+            category = Category(
+                name="Movie",
+                description="descrição",
+                is_activate=True,
+                created_at=created_at,
+            )
+            self.assertEqual(category.name, "Movie")
+            self.assertEqual(category.description, "descrição")
+            self.assertTrue(category.is_activate)
+            self.assertEqual(category.created_at, created_at)
 
     def test_if_created_at_is_generated_in_constructor(self):
-        category1 = Category(name="Movie1")
-        category2 = Category(name="Movie2")
-        self.assertNotEqual(
-            category1.created_at.timestamp(), category2.created_at.timestamp()
-        )
+        with patch.object(Category, "validate"):
+            category1 = Category(name="Movie1")
+            category2 = Category(name="Movie2")
+            self.assertNotEqual(
+                category1.created_at.timestamp(), category2.created_at.timestamp()
+            )
 
     def test_is_imutable(self):
-        with self.assertRaises(FrozenInstanceError):
-            category = Category(name="Movie")
-            category.name = "Fake Name"
+        with patch.object(Category, "validate"):
+            with self.assertRaises(FrozenInstanceError):
+                category = Category(name="Movie")
+                category.name = "Fake Name"
 
     def test_method_update(self):
-        category = Category(name="name", description="description")
-        category.update(name="new name", description="new description")
-        self.assertEqual(category.name, "new name")
-        self.assertEqual(category.description, "new description")
+        with patch.object(Category, "validate"):
+            category = Category(name="name", description="description")
+            category.update(name="new name", description="new description")
+            self.assertEqual(category.name, "new name")
+            self.assertEqual(category.description, "new description")
 
     def test_method_desactivate(self):
-        category = Category(name="name", description="descriptioi")
-        category.deactivate()
-        self.assertFalse(category.is_activate)
+        with patch.object(Category, "validate"):
+            category = Category(name="name", description="descriptioi")
+            category.deactivate()
+            self.assertFalse(category.is_activate)
 
     def test_method_activate(self):
-        category = Category(name="name", description="descriptioi", is_activate=False)
-        category.activate()
-        self.assertTrue(category.is_activate)
+        with patch.object(Category, "validate"):
+            category = Category(
+                name="name", description="descriptioi", is_activate=False
+            )
+            category.activate()
+            self.assertTrue(category.is_activate)
